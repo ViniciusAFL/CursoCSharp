984846afa8ab7adcc3b16ee407569311f52ad59f
VINICIUS ANDRE FERREIRA LUIZ <vinicius.afluiz@senacsp.edu.br>
2023-05-10 16:50:19 -0300

    10/05/2023 Segunda aula

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ namespace CursoCSharp {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Variaveis e Constantes - Fundamentos",VariaveisEConstantes.Executar },
-                {"Notação Ponto - Fundamentos",NotacaoPonto.Executar }
+                {"Notação Ponto - Fundamentos",NotacaoPonto.Executar },
+                {"Lendo Dados - Fundamentos",LendoDados.Executar },
+                {"Formatando Numeros - Fundamentos",FormatandoNumeros.Executar },
+                {"Operadores Aritmeticos - Fundamentos",OperadoresAritmeticos.Executar }
             });
 
             central.SelecionarEExecutar();
